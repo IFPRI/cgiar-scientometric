e5be2ec9662ce366d777880f2765cfc814a0f03e
Jawoo Koo <j.koo@cgiar.org>
2017-02-04 14:23:12 -0500

    Update text.js

--- text.js
+++ text.js
@@ -8,31 +8,7 @@
 _ifpritxt = {};
 
 // HOME TEXT
-_ifpritxt.home = "<h3>Who is CGIAR?</h3>
-
-<p>
-
-
-CGIAR is a global agricultural research partnership formed in 1971, on the basis of convincing evidence that agricultural science is a powerful instrument for combating hunger. CGIAR's research is dedicated to reducing poverty, enhancing food and nutrition security, and improving natural resources and ecosystem services. CGIAR proved that dramatic increases in agricultural productivity is possible through the widespread adoption of new, high-yielding crop varieties, combines with increased use of fertilizers, irrigation and other inputs, in what came to be celebrated as the Green Revolution. Its research is carried out by 15 CGIAR centers in close collaboration with hundreds of partners.
-</p>
-
-<h3>CGIAR Reform</h3>
-
-<p>
-Since early beginnings, the CGIAR System had evolved to become increasingly complicated with multiple donors, centers, crop and national priorities. In 2008, CGIAR donors and other stakeholders agreed to embark on a reform process to improve the engagement between stakeholders in international agricultural research for development – donors, researchers and beneficiaries – and to refocus CGIAR on the major global development challenges. The broad objectives were to integrate the work of the research centers, enhance collaboration with partners, ensure effective governance, and improve efficiency in providing and using resources. The reform process created a new governing structure centered around the CGIAR Consortium and launched 15 CGIAR Research Programs (CRPs) in 2010. 
-</p>
-
-<h3>Has the reform strengthened CGIAR's partnership?</h3>
-
-<p>
-One of the key features of the CGIAR reform was the strengthened culture of partnership both within CGIAR and between CGIAR and external entities. By applying the scientometric technique on the co-authorship of scientific research papers, this study examined if there has been any notable trend shift before and after the reform. Assuming the partnership is reflected in the co-authorship of research output, we hypothesized that there would have been more journal articles co-authored with multiple partners, both within CGIAR and external partners, if the reform indeed strengthened the partnership as intended.
-</p>
-
-<h3>Data and Methodology</h3>
-
-<p>
-To test the hypothesis, the bibliography of 12,050 SCI-indexed journal articles authored by CGIAR scientists, published between January 2000 to November 2016, was retrieved from Web of Science. The bibliography was analyzed to find shifting patterns in the proportion of papers from each center co-authored with other CGIAR centers and external partners. Papers with ambiguous and non-center-specific authorship were excluded in this analysis. Additionally, trends of research topic keywords were also analyzed to examine if papers with specific topics are more frequently developed from partnerships.
-</p>";
+_ifpritxt.home = "<h3>Who is CGIAR?</h3><p>CGIAR is a global agricultural research partnership formed in 1971, on the basis of convincing evidence that agricultural science is a powerful instrument for combating hunger. CGIAR's research is dedicated to reducing poverty, enhancing food and nutrition security, and improving natural resources and ecosystem services. CGIAR proved that dramatic increases in agricultural productivity is possible through the widespread adoption of new, high-yielding crop varieties, combines with increased use of fertilizers, irrigation and other inputs, in what came to be celebrated as the Green Revolution. Its research is carried out by 15 CGIAR centers in close collaboration with hundreds of partners.</p><h3>CGIAR Reform</h3><p>Since early beginnings, the CGIAR System had evolved to become increasingly complicated with multiple donors, centers, crop and national priorities. In 2008, CGIAR donors and other stakeholders agreed to embark on a reform process to improve the engagement between stakeholders in international agricultural research for development – donors, researchers and beneficiaries – and to refocus CGIAR on the major global development challenges. The broad objectives were to integrate the work of the research centers, enhance collaboration with partners, ensure effective governance, and improve efficiency in providing and using resources. The reform process created a new governing structure centered around the CGIAR Consortium and launched 15 CGIAR Research Programs (CRPs) in 2010.</p><h3>Has the reform strengthened CGIAR's partnership?</h3><p>One of the key features of the CGIAR reform was the strengthened culture of partnership both within CGIAR and between CGIAR and external entities. By applying the scientometric technique on the co-authorship of scientific research papers, this study examined if there has been any notable trend shift before and after the reform. Assuming the partnership is reflected in the co-authorship of research output, we hypothesized that there would have been more journal articles co-authored with multiple partners, both within CGIAR and external partners, if the reform indeed strengthened the partnership as intended.</p><h3>Data and Methodology</h3><p>To test the hypothesis, the bibliography of 12,050 SCI-indexed journal articles authored by CGIAR scientists, published between January 2000 to November 2016, was retrieved from Web of Science. The bibliography was analyzed to find shifting patterns in the proportion of papers from each center co-authored with other CGIAR centers and external partners. Papers with ambiguous and non-center-specific authorship were excluded in this analysis. Additionally, trends of research topic keywords were also analyzed to examine if papers with specific topics are more frequently developed from partnerships.</p>";
 
 // YEARLY COLLABORATION TEXT
 _ifpritxt.collaborationsyear = "The <i>Chord</i> visualization below shows organization co-authorship between academic publications in the CGIAR system.  Hover over a organization name to highlight the links to other countries.  Hover over a link to view the number of co-author publications.  Move the slider on the right to change the year of publication.";
